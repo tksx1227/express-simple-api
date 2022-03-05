2e45c814a4e4a8c9446e33df45c83eace8b9b4fb
tomori <tmrtkm1227tksx@gmail.com>
2022-03-05 11:26:08 +0900

    [add] app.ts

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+import express, { Application } from "express"
+import compression from "compression"
+import cors from "cors"
+import morgan from "morgan"
+import { Sequelize, Model, DataTypes } from "sequelize"
+import Controller from "@/utils/interfaces/controller.interface"
+import ErrorMiddleware from "@/middleware/error.middleware"
+import helmet from "helmet"
+
+class App {
+    public express: Application
+    public port: number
+
+    constructor(controllers: Controller[], port: number) {
+        this.express = express()
+        this.port = port
+
+        this.initializeDatabaseConnection()
+        this.initializeMiddleware()
+        this.initializeControllers(controllers)
+        this.initializeErrorHandling()
+    }
+
+    private initializeMiddleware(): void {
+        this.express.use(helmet())
+        this.express.use(cors())
+        this.express.use(morgan("dev"))
+        this.express.use(express.json())
+        this.express.use(express.urlencoded({ extended: false }))
+        this.express.use(compression())
+    }
+
+    private initializeControllers(controllers: Controller[]): void {
+        controllers.forEach((controller: Controller) => {
+            this.express.use("/api", controller.router)
+        })
+    }
+
+    private initializeErrorHandling(): void {
+        this.express.use(ErrorMiddleware)
+    }
+
+    private initializeDatabaseConnection(): void {
+        // Connect to MySQL here
+    }
+
+    public listen(): void {
+        this.express.listen(this.port, () => {
+            console.log(`App listening on port ${this.port}`)
+        })
+    }
+}
+
+export default App
